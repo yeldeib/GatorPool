ca1587a0465e1ce2ec3ce4107c60daa853045001
skylarmccain <skycmccain@gmail.com>
2020-04-28 16:43:40 -0400

    added profiles and links on cards to access

--- gatorpool/src/Routes.js
+++ gatorpool/src/Routes.js
@@ -7,6 +7,8 @@ import Landing from "./components/Landing";
 import Signup from "./components/Signup"
 import Profile from "./components/Profile";
 import ProfileJack from "./components/ProfileJack";
+import ProfileJohhny from "./components/profileJohnny";
+import ProfileJoel from "./components/ProfileJoel";
 
 export default function Routes() {
     return (
@@ -27,10 +29,16 @@ export default function Routes() {
         <Route exact path="/profile">
           <Profile />
         </Route>
-        <Route exact path="/profileJack">
+        <Route exact path = "/profile1">
+          <ProfileJohhny/>
+        </Route>
+        <Route exact path="/profile2">
           <ProfileJack />
         </Route>
       </Switch>
+      <Route exact path='/profile3'>
+        <ProfileJoel />
+      </Route>
       </BrowserRouter>
     );
   }
---
+++ gatorpool/src/components/ProfileJoel/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Nav from '../Nav';
+import RideCard from '../RideCard';
+import ProRider from '../ProRider';
+import {Row, Col, Container} from 'reactstrap';
+import GatorLogo from '../Landing/GatorLogo.png'
+import '../Profile/Profile.css';
+
+class ProfileJoel extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            rides:[
+                {
+                    location: "Trader Joes",
+                    day_month: "2020-04-22",
+                    when: "10:30",
+                    drop: "11:45",
+                    driver: "Johny Appleseed",
+                    others: "George Washington, Tom Brady"
+                }
+            ],
+            currentRide:{}
+        }
+    }
+
+    render(){
+        let customCards = this.state.rides.map(newCard => {
+            return (
+                <Col sm="4">
+                    <ProRider newCard={newCard} />
+                </Col>
+            )
+        })
+        return(
+            <div>
+                <Nav />
+                <Container>
+                    <Row className="justify-content-md-center">
+                        <Col md="auto">
+                            <img className="proimg" src={GatorLogo} />  
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="justify-content-md-center">
+                            <h2 className="center">Swole Joel</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h2 className="driver">Driver</h2>
+                            <h2 className="rider">Rider</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="justify-content-md-center">
+                            <h5 className="center">Hey, I am a nutritional sciences and APK double major! I love working out and being outside!</h5>
+                        </Col>
+                    </Row>
+                </Container>
+                <h4>Upcoming Rides: </h4>
+                <Row>
+                    {customCards}
+                </Row>
+            </div>
+        )
+    }
+}
+export default ProfileJoel;
